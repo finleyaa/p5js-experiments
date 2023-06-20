04a0ab6870b3b772014604f477263d8a6a8bd38b
Finley Dabinett <finleytorterra@gmail.com>
2023-06-21 00:40:28 +0100

    changes

--- junodream_particles/particle.js
+++ junodream_particles/particle.js
@@ -1,5 +1,3 @@
-const sizeSpeed = 0.3
-
 class Particle {
   constructor (x, y) {
     this.pos = createVector(x, y)
@@ -11,30 +9,61 @@ class Particle {
     this.color = [Math.random() * 255, Math.random() * 255, Math.random() * 255]
     this.shrinking = false
     this.previousPositions = []
+
+    this.isLarge = false
+    if (Math.random() < 0.05) {
+      this.isLarge = true
+      this.maxSize = map(Math.random(), 0, 1, 200, 1000)
+      this.maxSpeed *= 2
+      this.isStationary = false
+    } else {
+      this.isStationary = Math.random() < 0.05 // 5% chance of being stationary which means it grows fully before moving
+    }
+
+    this.becomingBackground = false
+    this.isBackground = false
+
+    this.sizeSpeed = this.maxSize / 3 / 60
   }
 
   update () {
     if (!this.shrinking) {
-      this.size = Math.min(this.size + sizeSpeed, this.maxSize)
-      if (Math.random() < 0.005) {
+      this.size = Math.min(this.size + this.sizeSpeed, this.maxSize)
+      if (this.size >= this.maxSize && this.isLarge && Math.random() < 0.7 && !this.becomingBackground) {
+          this.becomingBackground = true
+          this.maxSize = Math.max(height, width)
+          this.sizeSpeed = this.maxSize / 3 / 60
+      } else if (this.size >= this.maxSize && this.becomingBackground) {
+        this.isBackground = true
+      }
+      
+      if (Math.random() < 0.005 && !this.isStationary && !this.becomingBackground) {
         this.shrinking = true
       }
     } else {
-      this.size -= sizeSpeed
+      this.size -= this.sizeSpeed
       if (this.size <= 0) {
         this.size = 0
       }
     }
+
+    if (this.isStationary && this.size >= this.maxSize) {
+      this.isStationary = false
+    }
+    if (this.isStationary) return
 
     this.vel.add(this.acc)
     this.vel.limit(this.maxSpeed)
     this.pos.add(this.vel)
     this.acc.mult(0)
 
-    if (this.pos.x > width) this.pos.x = 0
-    if (this.pos.x < 0) this.pos.x = width
-    if (this.pos.y > height) this.pos.y = 0
-    if (this.pos.y < 0) this.pos.y = height
+    if (this.isLarge) {
+      const buffer = this.maxSize * 3
+      if (this.pos.x > width + buffer) this.pos.x = -buffer
+      if (this.pos.x < -buffer) this.pos.x = width + buffer
+      if (this.pos.y > height + buffer) this.pos.y = -buffer
+      if (this.pos.y < -buffer) this.pos.y = height + buffer
+    }
 
     this.previousPositions.push(this.pos.copy())
     // higher speed and bigger size means a bigger trail
@@ -56,5 +85,12 @@ class Particle {
       strokeWeight(this.size - ((this.previousPositions.length - index) * 0.5))
       point(pos.x, pos.y)
     })
+  }
+
+  offscreen () {
+    const buffer = this.maxSize * 3
+    if (this.pos.x > width + buffer || this.pos.x < -buffer || this.pos.y > height + buffer || this.pos.y < -buffer) {
+      return true
+    }
   }
 }
--- junodream_particles/sketch.js
+++ junodream_particles/sketch.js
@@ -3,17 +3,42 @@ const BACKGROUND_START_COLOR = [255, 249, 222]
 const NOISE_SCALE = 0.01
 const NOISE_SPEED = 4
 const FLOW_FIELD_DENSITY = 10
+const PAPER_NOISE_DENSITY = 1
 
 let currentBgColor = BACKGROUND_START_COLOR
 const particles = []
+let paperImage = null
 
 function setup() {
   pixelDensity(1)
   createCanvas(window.innerWidth, window.innerHeight)
+  
+  const paperNoise = {}
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+      paperNoise[`${x},${y}`] = Math.random()
+    }
+  }
+  
+  const setupPaper = () => {
+    paperImage = createImage(width, height)
+    paperImage.loadPixels()
+    for (let x = 0; x < width; x += PAPER_NOISE_DENSITY) {
+      for (let y = 0; y < height; y += PAPER_NOISE_DENSITY) {
+        const noiseVal = paperNoise[`${x},${y}`]
+        const alpha = map(noiseVal, 0, 1, 0, 60)
+        paperImage.set(x, y, [255, 255, 255, alpha])
+      }
+    }
+    paperImage.updatePixels()
+  }
+
+  setupPaper()
   noiseDetail(8, 0.3)
 
   window.addEventListener('resize', () => {
     resizeCanvas(window.innerWidth, window.innerHeight)
+    setupPaper()
   })
 
   for (let i = 0; i < 4; i++) {
@@ -31,32 +56,25 @@ function draw() {
     text(frameRate().toFixed(2), 10, 20)
   }
 
-  // draw the noise
-  // loadPixels()
-  // for (let x = 0; x < width; x++) {
-  //   for (let y = 0; y < height; y++) {
-  //     const noiseVal = noise(x * NOISE_SCALE, y * NOISE_SCALE, frameCount * NOISE_SPEED * NOISE_SCALE)
-  //     const adjustedNoiseVal = map(noiseVal, 0, 1, 0, 0.5)
-  //     const color = map(adjustedNoiseVal, 0, 1, 0, 255)
-  //     set(x, y, [color, color, color, 255])
-  //   }
-  // }
-  // updatePixels()
-
-  if (Math.random() < 0.01 || particles.length <= 1) {
+  if (Math.random() < 0.02 || particles.length <= 1) {
     // add a new particle
     particles.push(new Particle(Math.random() * width, Math.random() * height))
   }
 
   particles.forEach(particle => {
     particle.update()
+    if (particle.isBackground) {
+      currentBgColor = particle.color
+      particle.size = 0
+      particle.shrinking = true
+    }
     particle.show()
     const noiseVal = noise((particle.pos.x + (frameCount * NOISE_SPEED)) * NOISE_SCALE, (particle.pos.y - (frameCount * NOISE_SPEED)) * NOISE_SCALE, frameCount * NOISE_SPEED * NOISE_SCALE)
     const angle = map(noiseVal, 0.5, 1, 0, TWO_PI)
     const vector = p5.Vector.fromAngle(angle)
     vector.setMag(1)
     particle.applyForce(vector)
-    if (particle.size === 0 && particle.shrinking) {
+    if (particle.size === 0 && particle.shrinking || particle.offscreen()) {
       // remove particle
       const index = particles.indexOf(particle)
       if (index > -1) {
@@ -64,4 +82,7 @@ function draw() {
       }
     }
   })
+
+  // draw noise faded on top
+  image(paperImage, 0, 0)
 }
